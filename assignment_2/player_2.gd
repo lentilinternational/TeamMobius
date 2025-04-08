a29d43a96ebb65a0c588056e7204d98fc65c2b66
lentilinternational <136150454+lentilinternational@users.noreply.github.com>
2025-04-08 13:50:04 +1000

    Completion of PR05
    I have added a camera that can move with the mouse, space is a jump, r is reset, go wild.

--- assignment_2/player_2.gd
+++ assignment_2/player_2.gd
@@ -1,8 +1,46 @@
 extends RigidBody3D
 
+var mouse_sensitivity := 0.001
+var twist_input := 0.0
+var pitch_input := 0.0
+var is_grounded := false
+
+@onready var twist_pivot := $twist_pivot
+@onready var pitch_pivot := $twist_pivot/pitch_pivot
+@onready var ground_check := $RayCast3D
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 func _process(delta: float) -> void:
-		var input := Vector3.ZERO
-		input.x = Input.get_axis("move_left2", "move_right2")
-		input.z = Input.get_axis("move_forward2", "move_down2")
-		
-		apply_central_force(input * 1200.0 * delta)
+	var input := Vector3.ZERO
+	input.x = Input.get_axis("move_left2", "move_right2")
+	input.z = Input.get_axis("move_forward2", "move_down2")
+	apply_central_force(twist_pivot.basis * input * 1200.0 * delta)
+
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+	if Input.is_action_just_pressed("ui_enter"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+	is_grounded = ground_check.is_colliding()
+	if is_grounded:
+		print("Player is on the ground")
+	else:
+		print("Player is in the air")
+
+	if Input.is_action_just_pressed("jump") and is_grounded:
+		apply_impulse(Vector3.UP * 5)
+
+	twist_pivot.rotate_y(twist_input)
+	pitch_pivot.rotate_x(pitch_input)
+	pitch_pivot.rotation.x = clamp(pitch_pivot.rotation.x, -0.5, 0.5)
+	twist_input = 0.0
+	pitch_input = 0.0
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			twist_input = -event.relative.x * mouse_sensitivity
+			pitch_input = -event.relative.y * mouse_sensitivity
